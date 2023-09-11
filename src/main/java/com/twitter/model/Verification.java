1a3fde8b6a8c9fdcd69caa7cb3b9c7f04ffd0161
Shubham <shubhamyadav32100@gmail.com>
2023-09-11 21:54:07 +0530

    authentication added

---
+++ src/main/java/com/twitter/model/Verification.java
@@ -0,0 +1,18 @@
+package com.twitter.model;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+
+@Data
+public class Verification {
+
+	private boolean status = false;
+
+	private LocalDateTime startedAt;
+
+	private LocalDateTime endsAt;
+	
+	private String planType;
+}
